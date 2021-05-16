8f6ba741874c1b2f4f7f29456d840a1be79378e0
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2021-05-16 15:16:43 +0300

    simplify and stage for tiling based on last action

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,103 +1,110 @@
-function newSlotPosition(workspace, client, numberXslots, numberYslots, x, y, xSlotToFill, ySlotToFill) {
-    var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
-    var width;
-    if (x == numberXslots) {
-        width = Math.floor(maxArea.width / numberXslots);
-    } else {
-        width = Math.ceil(maxArea.width / numberXslots);
-    }
+function newSlotPosition(workspace, client, action) {
+  numberXslots = action[0];
+  numberYslots = action[1];
+  x = action[2];
+  y = action[3];
+  xSlotToFill = action[4];
+  ySlotToFill = action[5];
 
-    var height;
-    if (y == numberYslots) {
-        height = Math.floor(maxArea.height / numberYslots);
-    } else {
-        height = Math.ceil(maxArea.height / numberYslots);
-    }
+  var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
+  var width;
+  if (x == numberXslots) {
+    width = Math.floor(maxArea.width / numberXslots);
+  } else {
+    width = Math.ceil(maxArea.width / numberXslots);
+  }
 
-    var newX = maxArea.x + width * x;
-    var newY = maxArea.y + height * y;
-    return [newX, newY, width * xSlotToFill, height * ySlotToFill]
+  var height;
+  if (y == numberYslots) {
+    height = Math.floor(maxArea.height / numberYslots);
+  } else {
+    height = Math.ceil(maxArea.height / numberYslots);
+  }
+
+  var newX = maxArea.x + width * x;
+  var newY = maxArea.y + height * y;
+
+  return {
+    x: newX,
+    y: newY,
+    width: width * xSlotToFill,
+    height: height * ySlotToFill
+  };
 }
-function reposition(client, newX, newY, w, h) {
-    client.geometry = {
-        x: newX,
-        y: newY,
-        width: w,
-        height: h
-    };
+
+var previousClient;
+var previousAction;
+function move(workspace, action) {
+  var client = workspace.activeClient;
+  if (client.moveable) {
+    newGeometry = newSlotPosition(workspace, client, action);
+    if (client === previousClient && previousAction === action) {
+      newGeometry = figureNewAction(workspace, client, action);
+    }
+    client.geometry = newGeometry;
+  }
+  previousClient = client;
+  previousAction = action;
 }
 
-function move(workspace, numberXslots, numberYslots, x, y, xSlotToFill, ySlotToFill) {
-    var client = workspace.activeClient;
-    if (client.moveable) {
-        arr = newSlotPosition(workspace, client, numberXslots, numberYslots, x, y, xSlotToFill, ySlotToFill);
-        var newX = arr[0],
-            newY = arr[1],
-            w = arr[2],
-            h = arr[3];
-        reposition(client, newX, newY, w, h)
-    }
+function figureNewAction(workspace, client, action) {
+
+  return newSlotPosition(workspace, client, action);;
 }
 
 function center(workspace) {
-    var client = workspace.activeClient;
-    if (client.moveable) {
-        var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
-        var newX = maxArea.x + ((maxArea.width - client.width) / 2);
-        var newY = maxArea.y + ((maxArea.height - client.height) / 2);
-        reposition(client, newX, newY, client.width, client.height)
-    }
+  var client = workspace.activeClient;
+  if (client.moveable) {
+    var maxArea = workspace.clientArea(KWin.MaximizeArea, client);
+    var newX = maxArea.x + ((maxArea.width - client.width) / 2);
+    var newY = maxArea.y + ((maxArea.height - client.height) / 2);
+    client.geometry = {
+      x: newX,
+      y: newY,
+      width: client.width,
+      height: client.width
+    };
+  }
 }
 
 
-// function isInPosition(workspace, numberXslots, numberYslots, x, y, xSlotToFill, ySlotToFill) {
-//     var client = workspace.activeClient;
-//     if (client.moveable) {
-//         arr = getPosition(workspace, client, numberXslots, numberYslots, x, y, xSlotToFill, ySlotToFill);
-//         var newX = arr[0],
-//             newY = arr[1],
-//             w = arr[2],
-//             h = arr[3];
-//         return (client.x == newX && client.y == newY && client.width == w && client.height == h);
-//     }
-//     return false;
-// }
+
 
 // GRID 3x2
 registerShortcut("MoveWindowToUpLeft3x2", "UltrawideWindows: Move Window to up-left (3x2)", "Meta+Num+7", function () {
-    move(workspace, 3, 2, 0, 0, 1, 1)
+    move(workspace, [3, 2, 0, 0, 1, 1])
 });
 
 registerShortcut("MoveWindowToUpCenter3x2", "UltrawideWindows: Move Window to up-center (3x2)", "Meta+Num+8", function () {
-    move(workspace, 3, 2, 1, 0, 1, 1)
+    move(workspace, [3, 2, 1, 0, 1, 1])
 });
 
 registerShortcut("MoveWindowToUpRight3x2", "UltrawideWindows: Move Window to up-right (3x2)", "Meta+Num+9", function () {
-    move(workspace, 3, 2, 2, 0, 1, 1)
+    move(workspace, [3, 2, 2, 0, 1, 1])
 });
 
 registerShortcut("MoveWindowToDownLeft3x2", "UltrawideWindows: Move Window to down-left (3x2)", "Meta+Num+1", function () {
-    move(workspace, 3, 2, 0, 1, 1, 1)
+    move(workspace, [3, 2, 0, 1, 1, 1])
 });
 
 registerShortcut("MoveWindowToDownCenter3x2", "UltrawideWindows: Move Window to down-center (3x2)", "Meta+Num+2", function () {
-    move(workspace, 3, 2, 1, 1, 1, 1)
+    move(workspace, [3, 2, 1, 1, 1, 1])
 });
 
 registerShortcut("MoveWindowToDownRight3x2", "UltrawideWindows: Move Window to down-right (3x2)", "Meta+Num+3", function () {
-    move(workspace, 3, 2, 2, 1, 1, 1)
+    move(workspace, [3, 2, 2, 1, 1, 1])
 });
 
 registerShortcut("MoveWindowToLeftHeight3x2", "UltrawideWindows: Move Window to left-height (3x2)", "Meta+Num+4", function () {
-    move(workspace, 3, 1, 0, 0, 1, 1)
+    move(workspace, [3, 1, 0, 0, 1, 1])
 });
 
 registerShortcut("MoveWindowToCenterHeight3x2", "UltrawideWindows: Move Window to center-height (3x2)", "Meta+Num+5", function () {
-    move(workspace, 3, 1, 1, 0, 1, 1)
+    move(workspace, [3, 1, 1, 0, 1, 1])
 });
 
 registerShortcut("MoveWindowToRightHeight3x2", "UltrawideWindows: Move Window to right-height (3x2)", "Meta+Num+6", function () {
-    move(workspace, 3, 1, 2, 0, 1, 1)
+    move(workspace, [3, 1, 2, 0, 1, 1])
 });
 
 // GRID 2x2
